6da5b34d93393ba022ed4c2e02fc14cf0583c829
gimura2022 <gimura0001@gmail.com>
2024-11-26 21:39:06 +0300

    moved level names to static global variables

--- src/glog.c
+++ src/glog.c
@@ -8,6 +8,15 @@
 
 static const char* default_format = "[%s][%s][%s] %s";
 
+static const char* chaos_name       = "CHAOS";
+static const char* trace_name       = "TRACE";
+static const char* debug_name       = "DEBUG";
+static const char* info_name        = "INFO";
+static const char* warn_name        = "WARN";
+static const char* error_name       = "ERROR";
+static const char* die_name         = "DIE";
+static const char* unreachable_name = "UNREACHABLE";
+
 struct glog__logging_level glog__chaos_level       = {0};
 struct glog__logging_level glog__trace_level       = {0};
 struct glog__logging_level glog__debug_level       = {0};
@@ -22,14 +31,14 @@ static void unreachable_handler(void);
 
 void glog__init(void)
 {
-	glog__chaos_level       = (struct glog__logging_level) {.level = -300, .handler = NULL, .name                = "CHAOS"};
-	glog__trace_level       = (struct glog__logging_level) {.level = -200, .handler = NULL, .name                = "TRACE"};
-	glog__debug_level       = (struct glog__logging_level) {.level = -100, .handler = NULL, .name                = "DEBUG"};
-	glog__info_level        = (struct glog__logging_level) {.level = 0, .handler    = NULL, .name                = "INFO"};
-	glog__warn_level        = (struct glog__logging_level) {.level = 100, .handler  = NULL, .name                = "WARN"};
-	glog__error_level       = (struct glog__logging_level) {.level = 200, .handler  = NULL, .name                = "ERROR"};
-	glog__die_level         = (struct glog__logging_level) {.level = 1000, .handler = die_handler, .name         = "DIE"};
-	glog__unreachable_level = (struct glog__logging_level) {.level = 2000, .handler = unreachable_handler, .name = "UNREACHABLE"};
+	glog__chaos_level       = (struct glog__logging_level) {.level = -300, .handler = NULL, .name                = chaos_name};
+	glog__trace_level       = (struct glog__logging_level) {.level = -200, .handler = NULL, .name                = trace_name};
+	glog__debug_level       = (struct glog__logging_level) {.level = -100, .handler = NULL, .name                = debug_name};
+	glog__info_level        = (struct glog__logging_level) {.level = 0, .handler    = NULL, .name                = info_name};
+	glog__warn_level        = (struct glog__logging_level) {.level = 100, .handler  = NULL, .name                = warn_name};
+	glog__error_level       = (struct glog__logging_level) {.level = 200, .handler  = NULL, .name                = error_name};
+	glog__die_level         = (struct glog__logging_level) {.level = 1000, .handler = die_handler, .name         = die_name};
+	glog__unreachable_level = (struct glog__logging_level) {.level = 2000, .handler = unreachable_handler, .name = unreachable_name};
 }
 
 void glog__logger_from_prefix(struct glog__logger* logger, const char* prefix)
